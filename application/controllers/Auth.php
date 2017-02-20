72b5e4602e56f35da448304db4fd6080f23ecd84
Ardyanto Hermawan <ardyanto_hermawan@yahoo.com>
2017-02-20 20:31:24 +0700

    add bootstrap, plugins and divided into section

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,9 +3,52 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 
-	public function index()
-	{
+	function __construct(){
+		parent::__construct();
 		$this->load->library('template_engine');
+	}
+
+	public function index( ){
 		echo $this->template_engine->render('auth::profile', ['name' => 'Jonathan']);
+	}
+
+	public function form() {
+		echo $this->template_engine->render('auth::form', ['name' => 'Jonathan']);
+	}
+
+	public function getData(){
+
+		$draw = $_REQUEST['draw'];
+		$length = $_REQUEST['length'];
+		$start = $_REQUEST['start'];
+		$search = $_REQUEST['search']["value"];
+
+		$total = $this->db->count_all_results("villages");
+
+		$output = array();
+		$output['draw']=$draw;
+		$output['recordsTotal']=$output['recordsFiltered']=$total;
+		$output['data']=array();
+
+		if ($search != ""){
+			$this->db->like("name", $search);
+		}
+		$this->db->limit($length, $start);
+		$this->db->order_by('name', 'DESC');
+		$query = $this->db->get('villages');
+
+		if ($search != ""){
+			$this->db->like("name", $search);
+			$jum = $this->db->get('villages');
+			$output['recordsTotal'] = $output['recordsFiltered'] = $jum->num_rows();
+		}
+
+		$nomor_urut = $start+1;
+		foreach ($query->result_array() as $desa) {
+			$output['data'][] = array($nomor_urut, $desa['name']);
+			$nomor_urut++;
+		}
+
+		echo json_encode($output);
 	}
 }
---
+++ application/views/templates/base.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>PGGK | Data Tables</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="/assets<?=$this->asset('/css/bootstrap.min.css')?>" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="/assets<?=$this->asset('/css/AdminLTE.css')?>" />
+  <link rel="stylesheet" href="/assets<?=$this->asset('/css/skins/_all-skins.min.css')?>" />
+
+  <?=$this->section('style')?>
+  
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+
+    <?=$this->insert('header')?>
+
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+
+    <?=$this->insert('sidebar')?>
+
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+
+    <?=$this->section('container')?>
+
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+
+    <?=$this->insert('footer')?>
+
+  </footer>
+
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<script src="/assets<?=$this->asset('/plugins/jQuery/jquery-2.2.3.min.js')?>"></script>
+<script src="/assets<?=$this->asset('/js/bootstrap.min.js')?>"></script>
+<script src="/assets<?=$this->asset('/plugins/slimScroll/jquery.slimscroll.min.js')?>"></script>
+<script src="/assets<?=$this->asset('/plugins/fastclick/fastclick.js')?>"></script>
+<script src="/assets<?=$this->asset('/js/app.min.js')?>"></script>
+<script src="/assets<?=$this->asset('/js/demo.js')?>"></script>
+
+<?=$this->section('javascript')?>
+
+</body>
+</html>
